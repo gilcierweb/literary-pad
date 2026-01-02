663a14a606b970665e27dde2ff0e7dc30343e090
gilcierweb <gilcier06@yahoo.com.br>
2026-01-02 08:47:27 -0300

    add flyonui config

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,28 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+// FlyonUI
+// import "flyonui/flyonui"
+import "flyonui/dist/dropdown"
+import "flyonui/dist/accordion"
+import "flyonui/dist/carousel"
+import "flyonui/dist/remove-element"
+import "flyonui/dist/tabs"
+import "flyonui/dist/overlay"
+import "flyonui/dist/"
+
+document.addEventListener('turbo:load', function() {
+    // Load plugins preline flyonui
+    HSDropdown.autoInit();
+    HSAccordion.autoInit();
+    HSCarousel.autoInit();
+    HSRemoveElement.autoInit();
+    HSTabs.autoInit();
+    HSOverlay.autoInit();
+    window.HSStaticMethods?.autoInit();
+    // HSOverlay.autoInit();
+    // HSSelect.autoInit();
+
+
+});
