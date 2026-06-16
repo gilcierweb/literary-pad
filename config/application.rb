358d3631225390670bab06933a3195a45267eff5
gilcierweb <gilcier06@yahoo.com.br>
2026-06-15 22:44:02 -0300

    add i18n keys on views

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,15 @@ module LiteraryPad
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
+    config.time_zone = "America/Sao_Paulo"
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    config.i18n.enforce_available_locales = true
+    config.i18n.available_locales = [:"pt-BR", :pt, :en, :es, :fr]
+    config.i18n.default_locale = :"pt-BR"
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
+    config.i18n.fallbacks = true
+    
   end
 end
