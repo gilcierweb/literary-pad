db56a2bcae0d9880c0ac06b977b6f363075a6c73
gilcierweb <gilcier06@yahoo.com.br>
2026-06-15 21:49:24 -0300

    add layout auth users views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,19 @@ class ApplicationController < ActionController::Base
   # allow_browser versions: :modern
 
   include Pagy::Method
+  layout :layout_by_resource
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, alert: exception.message
+  end
+
+  protected
+
+  def layout_by_resource
+    if devise_controller?
+      "auth"
+    else
+      "application"
+    end
   end
 end
